ad9566bcf83f793df3c181b7a6945cace6095516
王鹏 <wangpeng9812@126.com>
2019-12-19 23:44:41 +0800

    跨域过滤器

---
+++ wp-api-gateway/src/main/java/com/njucm/config/CorFilter.java
@@ -0,0 +1,60 @@
+package com.njucm.config;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class CorFilter extends ZuulFilter {
+
+    private Logger logger = LoggerFactory.getLogger(CorFilter.class);
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        logger.info("method: {}", request.getMethod());
+//        if (request.getMethod().equals(RequestMethod.OPTIONS)) {
+//            return true;
+//        }
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        logger.info("***************corFilter run***************");
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletResponse response = ctx.getResponse();
+        HttpServletRequest request = ctx.getRequest();
+        response.setHeader("Access-Control-Allow-Origin",request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Credentials","true");
+        response.setHeader("Access-Control-Allow-Headers","authorization, content-type");
+        response.setHeader("Access-Control-Allow-Methods","POST,GET");
+        response.setHeader("Access-Control-Expose-Headers","X-forwared-port, X-forwarded-host");
+        response.setHeader("Vary","Origin,Access-Control-Request-Method,Access-Control-Request-Headers");
+        //不再路由
+//        ctx.setSendZuulResponse(false);
+//        ctx.setResponseStatusCode(200);
+
+        logger.info("***************corFilter end***************");
+        return null;
+
+    }
+}
